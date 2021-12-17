e18b3f7f6e6bd3079ec88c074167b421691c6326
Mael-Kehl <artyworldyah@gmail.com>
2021-12-18 00:41:40 +0100

    fixing all

--- js/app.js
+++ js/app.js
@@ -1,14 +1,16 @@
 let checkbox = document.querySelector("input[name=ldmod]");
 
 
-const Gauche = document.querySelector(".Gauche");
-const Droite = document.querySelector(".Droite");
+const gauche = document.querySelector("#gauche");
+const droite = document.querySelector("#droite");
 const para = document.querySelector(".controls");
 const imgs = document.querySelector(".icon");
-const fl1 = document.querySelector(".fl1");
-const fl2 = document.querySelector(".fl2");
-const fl3 = document.querySelector(".fl3");
-const fl4 = document.querySelector(".fl4");
+
+//Arrow up arrow left arrow right arrow down
+const au = document.getElementById("arrow-up");
+const al = document.getElementById("arrow-left");
+const ar = document.getElementById("arrow-right");
+const ad = document.getElementById("arrow-down");
 
 
 
@@ -16,48 +18,29 @@ const colors = ['#2A2A2A', 'white', 'black']
 
 checkbox.addEventListener( 'change', function() {
     if(this.checked) {
-
-
-
-
-        console.log('checked')
         document.body.style.backgroundColor = colors[0];
         document.body.style.color = colors[1];
-
         para.style.borderColor = colors[1];
-
-
-        Gauche.style.color = colors[1];
-        Droite.style.color = colors[1];
-
+        gauche.style.color = colors[1];
+        droite.style.color = colors[1];
         imgs.style.filter = "invert(100%)";
-        fl1.style.filter = "invert(100%)";
-        fl2.style.filter = "invert(100%)";
-        fl3.style.filter = "invert(100%)";
-        fl4.style.filter = "invert(100%)";
-
+        au.style.filter = "invert(100%)";
+        al.style.filter = "invert(100%)";
+        ar.style.filter = "invert(100%)";
+        ad.style.filter = "invert(100%)";
 
 
     } else {
-        console.log('not checked')
-        imgs.style.filter = "invert(0%)";
-        fl1.style.filter = "invert(0%)";
-        fl2.style.filter = "invert(0%)";
-        fl3.style.filter = "invert(0%)";
-        fl4.style.filter = "invert(0%)";
-        Gauche.style.color = colors[2];
-        Droite.style.color = colors[2];
-        document.body.style.backgroundColor = "#FFF"
-        document.body.style.color = '#000'
+        document.body.style.backgroundColor = colors[1];
+        document.body.style.color = colors[2];
         para.style.borderColor = colors[2];
-
-
-
-
-
-
-
-
+        gauche.style.color = colors[2];
+        droite.style.color = colors[2];
+        imgs.style.filter = "invert(0%)";
+        au.style.filter = "invert(0%)";
+        al.style.filter = "invert(0%)";
+        ar.style.filter = "invert(0%)";
+        ad.style.filter = "invert(0%)";
         
     }
 });
